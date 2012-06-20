d41dc39b0f2bea1f1aceb4c8f7b4c465f6a7daf4
Filipe W. Lima <fwl.ufpe@gmail.com>
2012-06-20 18:44:42 -0300

    Refatoramento dos filtros e dropdown.

--- public/javascripts/bootstrap-filter.js
+++ public/javascripts/bootstrap-filter.js
@@ -1,16 +1,16 @@
 !(function($) {
 
-  "use strict"
+  "use strict";
 
   var methods = {
 
     // Verifica os irmãos do checkbox.
     checkSiblings: function(checked, el) {
-      var parent = el.parent().parent(),
-          all = true
+      var parent = el.parent().parent()
+        , all = true
 
       el.siblings().each(function() {
-        return all = ($(this).children('input[type="checkbox"]').prop("checked") === checked)
+        return all = ($(this).children('input[type="checkbox"]').prop('checked') === checked)
       })
 
       if (all && checked) {
@@ -21,12 +21,12 @@
         methods.checkSiblings(checked, parent)
 
       } else if (all && !checked) {
-        parent.children('input[type="checkbox"]').prop("checked", checked)
-        parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0))
+        parent.children('input[type="checkbox"]').prop('checked', checked)
+        parent.children('input[type="checkbox"]').prop('indeterminate', (parent.find('input[type="checkbox"]:checked').length > 0))
         methods.checkSiblings(checked, parent)
 
       } else {
-        el.parents("li").children('input[type="checkbox"]').prop({
+        el.parents('li').children('input[type="checkbox"]').prop({
           indeterminate: true,
           checked: false
         })
@@ -36,8 +36,8 @@
     // Responsável pela parte de hierarquia de checkboxes.
     // Fonte: http://css-tricks.com/indeterminate-checkboxes/
     changeCheckBox: function(event) {
-      var checked = $(this).prop("checked"),
-          container = $(this).parent()
+      var checked = $(this).prop('checked')
+        , container = $(this).parent()
 
       container.find('input[type="checkbox"]').prop({
         indeterminate: false,
@@ -49,22 +49,23 @@
       // Adiciona a classe que escurece o texto dos itens marcados.
       event.data.filterGroup.find('input[type="checkbox"]').each(function() {
         var checkbox = $(this)
-        if (checkbox.prop("checked") || checkbox.prop("indeterminate")) {
-          checkbox.parent().addClass("filter-checked")
+
+        if (checkbox.prop('checked') || checkbox.prop('indeterminate')) {
+          checkbox.parent().addClass(event.data.settings.filterCheckedClass)
         } else {
-          checkbox.parent().removeClass("filter-checked")
+          checkbox.parent().removeClass(event.data.settings.filterCheckedClass)
         }
       })
 
       // Substitui o sub título do filtro.
-      $(this).trigger("replaceSubTitle", [event.data.filterGroup])
+      $(this).trigger('replaceSubTitle', [event.data.filterGroup])
     },
 
     // Desmarca totalmente um filtro.
-    uncheckFilter: function(filterGroup) {
-      filterGroup.find(".filter").removeClass("filter-active")
-      filterGroup.find(".filter-sub-title").text("")
-      filterGroup.find("li").removeClass("filter-checked")
+    uncheckFilter: function(filterGroup, settings) {
+      filterGroup.find('.' + settings.filterClass).removeClass(settings.filterActiveClass)
+      filterGroup.find('.' + settings.filterSubTitleClass).text('')
+      filterGroup.find('li').removeClass(settings.filterCheckedClass)
       filterGroup.find('input[type="checkbox"]').prop({
         checked: false,
         indeterminate: false
@@ -74,129 +75,145 @@
     // Substitui o sub título de um filtro de acordo com os checkboxes marcados.
     replaceSubTitle: function(event, filterGroup) {
       var checkedBoxes = filterGroup.find('input[type="checkbox"]:checked')
-      var filter = filterGroup.find(".filter").first()
-      var subTitleText = ""
+        , filter = filterGroup.find('.' + event.data.settings.filterClass)
+        , subTitleText = ''
 
       if (checkedBoxes.length === 1) {
-        subTitleText = checkedBoxes.siblings("label").text()
+        subTitleText = checkedBoxes.siblings('label').text()
       } else if (checkedBoxes.length > 1) {
         // Subtrai um pois não conta a opção "Todos".
-        subTitleText = (checkedBoxes.length - 1) + " opções selecionadas"
+        subTitleText = (checkedBoxes.length - 1) + ' opções selecionadas'
       }
 
       // Caso especial para o filtro de cursos e disciplinas.
-      if (filter.hasClass("filter-courses")) {
-        var coursesCheckBoxes = filterGroup.find('.filter-level-2 > li > input[type="checkbox"]')
+      if (filter.hasClass(event.data.settings.filterCoursesClass)) {
+        var coursesCheckBoxes = filterGroup.find('.' + event.data.settings.filterLevel2ItemClass + ' > input[type="checkbox"]')
+          , coursesCheckedBoxes = coursesCheckBoxes.filter(':checked')
+          , coursesUnCheckedBoxes = coursesCheckBoxes.filter(function(index) {
+            return $(this).prop('indeterminate')
+          })
 
         // Cursos.
-        var coursesCheckedBoxes = coursesCheckBoxes.filter(':checked')
         if (coursesCheckedBoxes.length === 1) {
-          subTitleText = coursesCheckedBoxes.siblings("label").text()
+          subTitleText = coursesCheckedBoxes.siblings('label').text()
         } else if (coursesCheckedBoxes.length > 1) {
-          subTitleText = coursesCheckedBoxes.length + " cursos selecionados"
+          subTitleText = coursesCheckedBoxes.length + ' cursos selecionados'
         }
 
         // Disciplinas.
-        var coursesUnCheckedBoxes = coursesCheckBoxes.filter(function(index) {
-          return $(this).prop("indeterminate")
-        })
         if (coursesUnCheckedBoxes.length >= 1) {
-          var disciplinesCheckedBoxes = filterGroup.find('.filter-level-3 > li > input[type="checkbox"]:checked')
+          var disciplinesCheckedBoxes = filterGroup.find('.' + event.data.settings.filterLevel3ItemClass + ' > input[type="checkbox"]:checked')
 
           if (disciplinesCheckedBoxes.length === 1) {
-            subTitleText = disciplinesCheckedBoxes.siblings("label").text()
+            subTitleText = disciplinesCheckedBoxes.siblings('label').text()
           } else if (disciplinesCheckedBoxes.length > 1) {
-            subTitleText = disciplinesCheckedBoxes.length + " disciplinas selecionadas"
+            subTitleText = disciplinesCheckedBoxes.length + ' disciplinas selecionadas'
           }
         }
       }
 
       // Adiciona a classe de filtro ativado.
       if (checkedBoxes.length >= 1) {
-        filter.addClass("filter-active")
+        filter.addClass(event.data.settings.filterActiveClass)
         // Desmarca os outros filtros.
         filterGroup.siblings().each(function() {
-          methods.uncheckFilter($(this))
+          methods.uncheckFilter($(this), event.data.settings)
         })
       } else {
-        filter.removeClass("filter-active")
+        filter.removeClass(event.data.settings.filterActiveClass)
       }
 
       // Trata os nomes grandes.
       if (subTitleText.length > 28) {
-        subTitleText = subTitleText.substring(0, 24) + "..."
+        subTitleText = subTitleText.substring(0, 24) + '...'
       }
 
-      filterGroup.find(".filter-sub-title").text(subTitleText)
+      filterGroup.find('.' + event.data.settings.filterSubTitleClass).text(subTitleText)
     },
 
     init: function(options) {
+      var settings = $.extend({
+          filterEverythingClass: 'filter-everything',
+          filterActiveClass: 'filter-active',
+          filterDropdownMenuClass: 'dropdown-menu',
+          filterClass: 'filter',
+          filterCoursesClass: 'filter-courses',
+          filterLevel2ItemClass: 'filter-level-2-item',
+          filterLevel3ItemClass: 'filter-level-3-item',
+          filterSubTitleClass: 'filter-sub-title',
+          filterCheckedClass: 'filter-checked'
+        }, options)
+
       return this.each(function() {
-        var filterToolBar = $(this)
-        var filterEverything = filterToolBar.find(".filter-everything").first()
+        var reduFilter = $(this)
+          , filterEverything = reduFilter.find('.' + settings.filterEverythingClass)
+          , dropdownFilters = filterEverything.parent().siblings()
+
         // O filtro "Tudo" começa ativo.
-        filterEverything.addClass("filter-active")
+        filterEverything.addClass(settings.filterActiveClass)
         // No estado ativado, desabilita todos os outros filtros.
-        filterEverything.click(function(e) {
-          filterEverything.parent().siblings().each(function() {
-            methods.uncheckFilter($(this))
+        filterEverything.on('click', function(e) {
+          dropdownFilters.each(function() {
+            methods.uncheckFilter($(this), settings)
           })
         })
 
         // Para cada filtro.
-        filterToolBar.children().each(function() {
+        dropdownFilters.each(function() {
           var filterGroup = $(this)
           // Para cada checkbox.
           filterGroup.find('input[type="checkbox"]').each(function() {
             var checkbox = $(this)
             // Inicia desmarcado.
-            checkbox.prop("checked", false)
+            checkbox.prop('checked', false)
             // Vincula o evento de substituir o sub título do filtro.
-            checkbox.bind("replaceSubTitle.filterToolBar", methods.replaceSubTitle)
-            checkbox.bind("change", {"filterGroup": filterGroup}, methods.changeCheckBox)
+            checkbox.on('replaceSubTitle.reduFilter', {settings: settings}, methods.replaceSubTitle)
+            checkbox.on('change', {settings: settings, filterGroup: filterGroup}, methods.changeCheckBox)
           })
         })
 
         // Impede que o dropdown feche ao ser clicado.
-        filterToolBar.find(".dropdown-menu").click(function(e) {
+        reduFilter.find('.' + settings.filterDropdownMenuClass).on('click', function(e) {
           e.stopPropagation()
         })
       })
     },
 
     // Alterna o estado de ativado dos filtros.
-    toggleState: function() {
+    toggleState: function(options) {
+      var settings = $.extend({
+          filterActiveClass: 'filter-active'
+        }, options)
+
       return this.each(function() {
         var filter = $(this)
+          , otherFilters = filter.siblings()
 
-        filter.click(function(e) {
+        filter.on('click', function(e) {
           e.preventDefault()
-
-          if (!filter.hasClass("filter-active")) {
-            filter.addClass("filter-active")
-          } else {
-            filter.removeClass("filter-active")
-          }
+          // Desativa os outros filtros.
+          otherFilters.removeClass(settings.filterActiveClass)
+          filter.toggleClass(settings.filterActiveClass)
         })
       })
     }
   }
 
-  $.fn.filterToolBar = function(method) {
+  $.fn.reduFilter = function(method) {
     if (methods[method]) {
       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1))
-    } else if (typeof method === "object" || !method) {
+    } else if (typeof method === 'object' || !method) {
       return methods.init.apply(this, arguments)
     } else {
-      $.error("O método " + method + " não existe em jQuery.filterToolBar")
+      $.error('O método ' + method + ' não existe em jQuery.reduFilter')
     }
   }
 
 }) (window.jQuery)
 
 $(function() {
-  // Alterna o estado de ativado nos filtros sem menu dropdown.
-  $(".filter:not(.dropdown-toggle)").filterToolBar('toggleState')
+  // Alterna o estado de ativado nos filtros sem dropdown.
+  $('.filter:not(.dropdown-toggle)').reduFilter('toggleState')
   // Adiciona os eventos dos filtros da visão geral.
-  $(".filter-toolbar").filterToolBar()
+  $('.filters-general-view').reduFilter()
 })
