23b7a283b76a9f2d98e299e84845febea6301508
Thaís Bione <thaisbione@gmail.com>
2012-10-05 08:03:32 -0300

    Alteração de estilos para 1.3.3

--- js/bootstrap-reply-status.js
+++ js/bootstrap-reply-status.js
@@ -1,6 +1,7 @@
  // Responder status
 $("a.reply-status, .cancel", ".statuses").live("click", function(e){
     e.preventDefault();
-    $(this).parents("ul:first").next(".create-response").slideToggle(150, "swing");
-    $(this).parents("ul:first").next(".create-response").find("textarea").focus();
+    var $lalala = $(this).parents("ul:first");
+    $lalala.parents().next(".create-response").slideToggle(150, "swing");
+    $lalala.parents().next(".create-response").find("textarea").focus();
 });
