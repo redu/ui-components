2070209c213d9e1c62582ac1c4b368760453177f
Filipe W. Lima <fwl.ufpe@gmail.com>
2012-11-16 17:54:37 -0300

    Adiciona funcionalidade de abrir modal por acesso a URL com seu id.

--- js/bootstrap-modal-redu.js
+++ js/bootstrap-modal-redu.js
@@ -178,4 +178,8 @@ $(function() {
   $('.modal').reduModal('fillHeight')
   $('.modal-scroll').reduModal('scrollArrow')
   $('.modal-fill-horizontal').reduModal('fillHorizontal')
+
+  // Abre uma modal caso seu id esteja na URL.
+  var $modal = $(/#[a-zA-Z-_\d]*/.exec(document.URL))
+  $modal.length != 0 && $modal.modal("show")
 })
