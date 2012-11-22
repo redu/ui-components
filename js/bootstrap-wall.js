c7381691647c93c205d2487555dd9cced504c55d
Thaís Bione <thaisbione@gmail.com>
2012-11-22 13:13:52 -0300

    Cria estilo para spans com propriedades de link, refatora JavaScript com a criação de funções, troca o uso de ancora por spans

--- js/bootstrap-wall.js
+++ js/bootstrap-wall.js
@@ -8,76 +8,94 @@ $(".responses .see-more").live("click", function(e){
     $responses.find("li").slideDown(150, 'swing');
     // Remove a opção de exibir todas as respostas
     $this.parent().remove();
-    // Troca o texto informando ao usuário que está visualizando todas as respostas
+    // Troca o texto para informar ao usuário que ele está visualizando todas as respostas
     $responses.find(".last-responses").html("Visualizando todas as respostas...");
 });
 
 // Exibe área de criação de respostas
-$(".actions .reply-status").live("click", function(e){
+$(".actions .reply-status span").live("click", function(e){
   var $this = $(this);
-
-  e.preventDefault()
 
   $this.parents(".subject-content").find(".create-response").slideDown(150, 'swing');
-
 });
 
-// Cancela a criação de respostas
-$(".status-buttons .cancel").live("click",function(e){
+// Esconde formulário para criação de respostas
+$(".create-response .status-buttons .cancel").live("click",function(e){
   var $this = $(this);
-  e.preventDefault()
 
   $this.parents(".create-response").slideUp(150, 'swing');
 });
 
-// Expansão do text-area para a criação de status
+// Expande o text-area para a criação de status
 $(".create-status textarea").live("click",function(e){
-
   var $textArea = $(this);
   var $button = $textArea.parent().find(".status-buttons");
 
-  $textArea.css("height",'122').slideDown(150,"swing");
+  $textArea.animate({ height: 136 }, 150);
   $button.slideDown(150, "swing");
 })
 
 // Cancelar a criação de status
 $(".create-status .status-buttons .cancel").live("click", function(e){
   e.preventDefault()
-
   var $this = $(this);
 
   $this.parents(".status-buttons").slideUp(150, 'swing');
-
-  $this.parents("form").find("textarea").css("height","30");
+  $this.parents("form").find("textarea").animate({ height: 30 }, 150);
 })
 
+// Agrupa respostas
+$.fn.groupResponses = function(opts){
+  return this.each(function(){
+    var $this = $(this);
+    var options = {
+      maxResponses : 3
+    }
+    $.extend(options, opts)
 
-$(function() {
-  // Agrupar respostas
-  $('.responses').each(function() {
-    var $responses = $(this).find("li:not(.show-responses)");
-    if ($responses.length > 3) {
-      $responses.filter(":lt(" + ($responses.length - 3) + ")").hide();
+    var $responses = $this.find("li:not(.show-responses)");
+    if ($responses.length > options.maxResponses) {
+      $responses.filter(":lt(" + ($responses.length - options.maxResponses) + ")").hide();
       $(this).find(".show-responses").show();
     }
-  })
+  });
+}
 
-  // Agrupar membros
-  $('.grouping-elements').each(function() {
+// Agrupa membros
+$.fn.groupMembers = function(opts){
+  return this.each(function(){
     var $this = $(this);
+    var options = {
+      elementWidth : 34,
+      elementHeight : 40
+    }
+    $.extend(options, opts)
+
     var $elements = $this.find("li");
     var width = $this.width();
-    var newHeight = (Math.ceil((($elements.length * 35) /  width)) *  40);
+    var newHeight = (Math.ceil((($elements.length * options.elementWidth) /  width)) *  options.elementHeight);
 
-    // Exibi os elementos agrupados
+    // Exibe os elementos agrupados
     $(".log .see-all").live("click",function(e) {
-      e.preventDefault()
-
-      $this.css("height", newHeight);
-      $elements.slideDown(150, 'swing');
-      $(this).remove()
+      $this.animate({ height: newHeight }, 150);
+      $(this).remove();
     });
   })
+}
+
+//Conta a quantidade de respostas de um post
+$.fn.countComments = function(){
+  return this.each(function(){
+    var $this = $(this);
+    var quantity = $this.find(".response").length;
+    $this.find(".see-more .count").html(quantity);
+  });
+};
+
+$(function() {
+  $('.responses').groupResponses();
+  $('.grouping-elements').groupMembers();
+  $(".responses").countComments();
 });
 
 
