1fd8ea5a97cdb900111d602a37a9dc5aba2f43f8
filipewl <fwl.ufpe@gmail.com>
2012-12-14 01:30:31 -0300

    Melhora o JS para tabelas com checkboxes para funcionar após AJAX.

--- js/bootstrap-tables.js
+++ js/bootstrap-tables.js
@@ -1,41 +1,99 @@
 !(function($) {
 
-  'use strict';
+  "use strict";
 
   var methods = {
 
-    init: function(options) {
-      var settings = $.extend({
-          'checkboxSelectedClass': 'table-checkbox-selected'
-        }, options)
+    // Altera o estado de seleção da linha do checkbox.
+    toggleState: function(options) {
+      var settings = $.extend({}, $.fn.reduTables.defaults, options)
 
-      return this.each(function() {
-        var checkbox = $(this),
-            row = checkbox.parents('tr')
+      var $checkbox = $(this)
+        , $row = $checkbox.closest("tr")
+        , $form = $checkbox.closest(settings.selectors.form)
 
-        if (checkbox.is(':checked')) {
-          row.addClass(settings.checkboxSelectedClass)
+      $row.toggleClass(settings.classes.checkboxSelected)
+      $form.trigger("verifySubmit")
+
+      return $checkbox
+    }
+
+    // Verifica se o botão de submissão deve ser ativado ou não.
+  , verifySubmit: function(options) {
+    var settings = $.extend({}, $.fn.reduTables.defaults, options)
+
+    var $form = $(this)
+      , $submit = $form.find('input[type="submit"]')
+      , $checkboxes = $form.find('input[type="checkbox"]')
+
+    $checkboxes.each(function() {
+      var $checkbox = $(this)
+
+      // Se o checkbox foi selecionado, abilita o submit.
+      if ($checkbox.is(":checked")) {
+        $submit.removeAttr("disabled")
+        return false
+      } else {
+        // Se foi o último a ser desmarcado, desabilita o submit.
+        if ($checkboxes.filter(":checked").length === 0) {
+          $submit.attr("disabled", "disabled")
         }
+      }
+    })
 
-        checkbox.on('change', function() {
-          row.toggleClass(settings.checkboxSelectedClass)
-        })
-      })
+    return $form
+  }
+
+  , init: function(options) {
+
     }
   }
 
   $.fn.reduTables = function(method) {
     if (methods[method]) {
-      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1))
-    } else if (typeof method === 'object' || !method) {
+      return methods[method].apply(this,
+        Array.prototype.slice.call(arguments, 1))
+    } else if (typeof method === "object" || !method) {
       return methods.init.apply(this, arguments)
     } else {
-      $.error('O método ' + method + ' não existe em jQuery.reduTables')
+      $.error("O método " + method + " não existe em jQuery.reduTables")
     }
   }
 
-}) (window.jQuery)
+  $.fn.reduTables.defaults = {
+    classes: {
+      checkboxSelected: "table-checkbox-selected"
+    }
+  , selectors: {
+      form: ".form-checklist"
+    }
+  }
 
-$(function() {
-  $('.form-checklist td input[type="checkbox"]').reduTables()
-})
+  $(function() {
+    var checkboxSelector = $.fn.reduTables.defaults.selectors.form +
+        ' td input[type="checkbox"]'
+      , $submit = $($.fn.reduTables.defaults.selectors.form +
+        ' input[type="submit"]').attr("disabled", "disabled")
+      , enableSubmit = false
+
+    $(document)
+      .on("change", checkboxSelector, function(e) {
+        $(this).reduTables("toggleState")
+      })
+      .on("verifySubmit", $.fn.reduTables.defaults.selectors.form,
+        function(e) {
+        $(this).reduTables("verifySubmit")
+      })
+
+    // FF caches os checkboxes selecionados após o page refresh.
+    $(checkboxSelector).filter(":checked").each(function() {
+      $(this).reduTables("toggleState")
+      enableSubmit = true
+    })
+
+    if (enableSubmit) {
+      $submit.removeAttr("disabled")
+    }
+  })
+
+}) (window.jQuery)
