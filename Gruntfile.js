cc7d529daff540a3aabeaf3d35cf4408d4c06bf7
Filipe W. Lima <fwl.ufpe@gmail.com>
2013-03-28 19:49:05 -0300

    Atualiza o Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,7 @@ module.exports = function(grunt) {
   ];
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
     lint: {
       files: files
@@ -60,5 +61,11 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('default', 'watch');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['watch']);
+
 };
