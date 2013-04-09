0e10a4634df536d95f41a2f7309a5e002d7a5ff2
Filipe W. Lima <fwl.ufpe@gmail.com>
2013-04-09 15:06:07 -0300

    Foca no campo de login quando 'Entrar no Redu' é clicado. Closes #46.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,7 @@ module.exports = function(grunt) {
   , 'js/bootstrap-spinners.js'
   , 'js/bootstrap-search-form.js'
   , 'js/bootstrap-wall.js'
+  , 'js/bootstrap-header.js'
   ];
 
   grunt.initConfig({
